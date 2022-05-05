a89a0bcf38a4db04e17b3c7afb85aa1e2379cb24
louisld <louisldthesamui@gmail.com>
2022-05-05 11:58:50 +0200

    refactoring views

--- pccp/admin/views.py
+++ pccp/admin/views.py
@@ -32,36 +32,7 @@ def projet_edit(slug):
     if form.validate_on_submit():
         form.populate_obj(p)
 
-        # Projet sections
-        del p.sections[:]
-        is_section = re.compile(r"title-([0-9]+)")
-        for field in request.form:
-            res = is_section.search(field) 
-            if res is not None:
-                ps = ProjetSection()
-                ps.title = request.form["title-" + res.group(1)]
-                ps.content = request.form["content-" + res.group(1)]
-                p.sections.append(ps)
-                db.session.add(ps)
-
-        db.session.add(p)
-        db.session.commit()
-
-        # Cover image
-        if form.cover_img.data:
-            img_data = request.files[form.cover_img.name].read()
-            file_path = os.path.join(img_folder, p.slug + '_cover.jpg')
-            if not os.path.exists(img_folder):
-                os.mkdir(img_folder)
-            open(file_path, "wb").write(img_data)
-
-        # Thumbnail
-        if form.thumbnail.data:
-            img_data = request.files[form.thumbnail.name].read()
-            file_path = os.path.join(img_folder, p.slug + '_thumbnail.jpg')
-            if not os.path.exists(img_folder):
-                os.mkdir(img_folder)
-            open(file_path, "wb").write(img_data)
+        submit_project_form(p, form, request)
         return redirect(url_for('admin.projet_edit', slug=p.slug))
 
     return render_template(
@@ -79,34 +50,40 @@ def projet_new():
         form.populate_obj(p)
         img_folder = 'static/img/projets/' + p.slug + '/'
 
-        # Projet sections
-        is_section = re.compile(r"title-([0-9]+)")
-        for field in request.form:
-            res = is_section.search(field) 
-            if res is not None:
-                ps = ProjetSection()
-                ps.title = request.form["title-" + res.group(1)]
-                ps.content = request.form["content-" + res.group(1)]
-                p.sections.append(ps)
-                db.session.add(ps)
-
-        db.session.add(p)
-        db.session.commit()
-        if form.cover_img.data:
-            img_data = request.files[form.cover_img.name].read()
-            file_path = os.path.join(img_folder, p.slug + '_cover.jpg')
-            if not os.path.exists(img_folder):
-                os.mkdir(img_folder)
-            open(file_path, "wb").write(img_data)
-        # Thumbnail
-        if form.thumbnail.data:
-            img_data = request.files[form.thumbnail.name].read()
-            file_path = os.path.join(img_folder, p.slug + '_thumbnail.jpg')
-            if not os.path.exists(img_folder):
-                os.mkdir(img_folder)
-            open(file_path, "wb").write(img_data)
+        submit_project_form(p, form, request)
         return redirect(url_for('admin.projet_edit', slug=p.slug))
     return render_template(
         "admin/projet_new.html",
         form=form
     )
+
+def submit_project_form(p, form, request):
+    img_folder = 'static/img/projets/' + p.slug + '/'
+
+    # Projet sections
+    del p.sections[:]
+    is_section = re.compile(r"title-([0-9]+)")
+    for field in request.form:
+        res = is_section.search(field) 
+        if res is not None:
+            ps = ProjetSection()
+            ps.title = request.form["title-" + res.group(1)]
+            ps.content = request.form["content-" + res.group(1)]
+            p.sections.append(ps)
+            db.session.add(ps)
+
+    db.session.add(p)
+    db.session.commit()
+    if form.cover_img.data:
+        img_data = request.files[form.cover_img.name].read()
+        file_path = os.path.join(img_folder, p.slug + '_cover.jpg')
+        if not os.path.exists(img_folder):
+            os.mkdir(img_folder)
+        open(file_path, "wb").write(img_data)
+    # Thumbnail
+    if form.thumbnail.data:
+        img_data = request.files[form.thumbnail.name].read()
+        file_path = os.path.join(img_folder, p.slug + '_thumbnail.jpg')
+        if not os.path.exists(img_folder):
+            os.mkdir(img_folder)
+        open(file_path, "wb").write(img_data)
